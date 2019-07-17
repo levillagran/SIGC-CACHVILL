7d5c273dae4bcbc52a9cf7832028e54f7d00ace4
Lenin V. <leninsasos@hotmail.com>
2019-07-17 11:46:15 -0500

    creación proyecto v1

---
+++ SIGC-CACHVILL/src/main/java/ec/com/vill/cachvill/SigcCachvillApplication.java
@@ -0,0 +1,13 @@
+package ec.com.vill.cachvill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigcCachvillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigcCachvillApplication.class, args);
+	}
+
+}
